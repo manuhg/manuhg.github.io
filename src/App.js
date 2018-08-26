10e0f10cc026b1a5a6578cbaa3a2d4b22d3a8512
manu-spherical <manuhegdev@gmail.com>
2018-08-26 10:57:46 +0530

    resume

--- src/App.js
+++ src/App.js
@@ -1,81 +1,19 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
-import faLinkedin from '@fortawesome/fontawesome-free-brands/faLinkedin';
-import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
-import faStackOverflow from '@fortawesome/fontawesome-free-brands/faStackOverflow';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import Button from 'antd/lib/button';
-import dp from '1dp.jpg';
-import Resume from 'Resume.pdf';
-
-// import { Document, Page } from 'react-pdf';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from 'pages/Home';
+import resume_doc from 'Resume.pdf';
 
 import 'css/loader.css';
 import 'css/App.css';
-
 class App extends Component {
   render() {
-    // const Loader = () => (
-    //   <div>
-    //     <div className="lds-ring" style={{ alignSelf: 'center' }}>
-    //       <div /> <div /> <div /> <div />
-    //     </div>
-    //   </div>
-    // );
-    const links = [
-      ['https://github.com/manuhg', faGithub],
-      ['http://www.linkedin.com/in/manu-hegde', faLinkedin],
-      ['https://twitter.com/manuhegdev', faTwitter],
-      ['https://stackoverflow.com/users/1147882/manuhg', faStackOverflow],
-      ['mailto:me@manuhegde.in', faEnvelope],
-    ];
     return (
-      <div className="App container">
-        <div className="row">
-          <div className="col-12">
-            <img style={{ borderRadius: '50%', width: '150px' }} src={dp} alt="Manu Hegde" />
-            <h1>Manu Hegde</h1>
-            <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://dev.to/manuhg">
-                <img
-                  style={{ marginBottom: '10px', width: '25px' }}
-                  src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg"
-                  alt="Manu Hegde's DEV Profile"
-                />
-              </a>&nbsp;
-              {links.map((link, i) => (
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="mr-auto"
-                  key={i}
-                  href={link[0]}
-                >
-                  <FontAwesomeIcon icon={link[1]} size="2x" />&nbsp;
-                </a>
-              ))}
-            </div>
-          </div>
-          <div className="col-12">
-            <div className="text-center">
-              <div style={{ fontSize: '1em', padding: '2px' }}>
-                <br />
-                Student | Geek | Machine Learning Enthusiast
-              </div>
-              <div>&nbsp;</div>
-              <div>
-                <a href={Resume}>
-                  <Button>Resume</Button>
-                  {/* <Document file={Resume} onLoadSuccess={this.onDocumentLoad}>
-                    <Page pageNumber={1} />
-                  </Document> */}
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/resume" component={() => (window.location = resume_doc)} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/pages/Resume.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import resume_doc from 'Resume.pdf';
+
+class Resume extends Component {
+  constructor() {
+    super();
+    this.onDocumentLoad = this.onDocumentLoad.bind(this);
+  }
+  onDocumentLoad() {
+    this.setState({ toggle: 1 });
+  }
+  render() {
+    return <div>Hang on!</div>;
+  }
+  componentDidMount() {
+    this.props.history.push(resume_doc);
+  }
+}
+export default Resume;
