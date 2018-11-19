fc60879cc3d3aee2f5317c55d734212a81f87c92
manu-spherical <manuhegdev@gmail.com>
2018-11-19 11:35:51 +0530

    resume alternatives

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,84 +1,28 @@
 import React, { Component } from 'react';
-import { Document, Page } from 'react-pdf';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
-
+import ResumeReactEmbed from 'pages/ResumeReactEmbed';
 import resume_doc from 'Resume.pdf';
 
 class Resume extends Component {
-  constructor() {
-    super();
-    this.onDocumentLoad = this.onDocumentLoad.bind(this);
-    this.state = {
-      numPages: null,
-      pageNumber: 1,
-    };
-  }
-  onDocumentLoad = ({ numPages }) => {
-    this.setState({ numPages });
-  };
-
   render() {
-    const { pageNumber } = this.state; /*numPages */
     var w = window,
       d = document,
       documentElement = d.documentElement,
       body = d.getElementsByTagName('body')[0],
       width = w.innerWidth || documentElement.clientWidth || body.clientWidth,
       height = w.innerHeight || documentElement.clientHeight || body.clientHeight;
-    var value = 0;
-    // if (width > height) value = width * 0.98 * 1.29;
-    // else value = (height * 0.98) / 1.29;
-    value = width > height ? height : width;
     return (
       <div className="container-fluid mzpz">
-        <div className="row fp">
-          <div
-            style={{
-              position: 'fixed',
-              left: width > height ? 'auto' : '0',
-              top: '0',
-              zIndex: '10',
-            }}
-            className="col-12"
-          >
-            <a href={resume_doc}>
-              <button className="ant-btn ant-btn-primary ">
-                <FontAwesomeIcon icon={faDownload} />
-              </button>
-            </a>
-          </div>
-        </div>
         <div className="row">
           <div className="col-12">
-            <div id="popa" className="text-center">
-              <Document file={resume_doc} onLoadSuccess={this.onDocumentLoad}>
-                {width > height ? (
-                  <Page height={value} pageNumber={pageNumber} />
-                ) : (
-                  <Page width={value} pageNumber={pageNumber} />
-                )}
-              </Document>
-              {/* <p>
-          Page {pageNumber} of {numPages}
-        </p> */}
+            <div className="text-center">
+              <object height={height} width={width} data={resume_doc} type="application/pdf">
+                <ResumeReactEmbed />
+              </object>
             </div>
           </div>
         </div>
       </div>
     );
-  }
-
-  componentDidUpdate() {
-    setTimeout(
-      console.log(
-        document.getElementById('popa').clientHeight,
-        document.getElementById('popa').clientWidth
-      ),
-      600
-    );
-    //   this.props.history.push(resume_doc);
-    //   window.location = resume_doc;
   }
 }
 export default Resume;
