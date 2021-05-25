9e7f8a827e2aeaf98d3ce6f85b111db1ce57fd35
Manu Hegde <manuhegdev@gmail.com>
2021-05-25 07:53:03 +0530

    added some more links , ft, et & bq and updated red canary url

--- src/links.js
+++ src/links.js
@@ -3,324 +3,348 @@ export const links = [
     name: 'openai',
     url: 'https://openai.com/blog/',
     category: 'ML/DL',
-    frequency: 2
+    frequency: 2,
   },
   {
     name: 'decrypt',
     url: 'https://decrypt.co/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'facebook blog',
     url: 'https://about.fb.com/news/',
     category: 'ML/DL',
-    frequency: 2
+    frequency: 2,
   },
   {
     name: 'google ai blog',
     url: 'https://www.blog.google/',
     category: 'ML/DL',
-    frequency: 2
+    frequency: 2,
   },
   {
     name: 'fortune',
     url: 'https://fortune.com/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'gizmodo',
     url: 'https://gizmodo.com/',
     category: 'general',
-    frequency: 2
+    frequency: 2,
   },
   {
     name: 'inc',
     url: 'https://www.inc.com/',
     category: 'general',
-    frequency: 2
+    frequency: 2,
   },
   {
     name: 'skynet',
     url: 'https://www.skynettoday.com/',
     category: 'ML/DL',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'nature',
     url: 'https://www.nature.com/',
     category: 'general',
-    frequency: 2
+    frequency: 2,
   },
   {
     name: 'producthunt',
     url: 'https://www.producthunt.com/',
     category: 'general',
-    frequency: 2
+    frequency: 2,
   },
   {
     name: 'app.getpocket',
     url: 'https://app.getpocket.com/',
     category: 'general',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'kaggle',
     url: 'https://www.kaggle.com/discussion',
     category: 'daily',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'kaggle-blog',
     url: 'https://medium.com/kaggle-blog',
     category: 'ML/DL',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'scientificamerican',
     url: 'https://www.scientificamerican.com',
     category: 'general',
-    frequency: 2
+    frequency: 2,
   },
   {
     name: 'crazyaboutstartups',
     url: 'https://www.crazyaboutstartups.com/',
     category: 'general',
-    frequency: 2
+    frequency: 2,
   },
   {
     name: 'red-canary',
-    url: 'https://redcanary.com/',
+    url: 'https://redcanary.com/blog/',
     category: 'opsec',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'exploit-db',
     url: 'https://www.exploit-db.com/',
     category: 'opsec',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'securelist',
     url: 'https://securelist.com/',
     category: 'opsec',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'thehackernews',
     url: 'https://thehackernews.com/',
     category: 'opsec',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'techcrunch',
     url: 'https://techcrunch.com/',
     category: 'daily',
-    frequency: 1
+    frequency: 1,
+  },
+  {
+    name: 'news_ycombinator',
+    url: 'https://news.ycombinator.com/',
+    category: 'general',
+    frequency: 1,
+  },
+  {
+    name: 'bloombergquint',
+    url: 'https://www.bloombergquint.com/',
+    category: 'general',
+    frequency: 3,
+  },
+  {
+    name: 'financial times',
+    url: 'https://www.ft.com/',
+    category: 'general',
+    frequency: 3,
+  },
+  {
+    name: 'economic times',
+    url: 'https://economictimes.indiatimes.com/',
+    category: 'general',
+    frequency: 3,
   },
   {
     name: 'vocabulary',
     url: 'https://www.vocabulary.com/',
     category: 'errands',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'tweetdeck.twitter',
     url: 'https://tweetdeck.twitter.com/',
     category: 'daily',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'thereader.mitpress',
     url: 'https://thereader.mitpress.mit.edu/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'mail.google',
     url: 'https://mail.google.com/mail/u/0/#inbox',
     category: 'errands',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'mail.google',
     url: 'https://mail.google.com/mail/u/1/#inbox',
     category: 'errands',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'mail.google',
     url: 'https://mail.google.com/mail/u/2/#inbox',
     category: 'errands',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'mail.google',
     url: 'https://mail.google.com/mail/u/3/#inbox',
     category: 'errands',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'qz',
     url: 'https://qz.com/india/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'theverge',
     url: 'https://www.theverge.com/',
     category: 'daily',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'nuzzel',
     url: 'https://nuzzel.com/',
     category: 'daily',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'wired',
     url: 'https://www.wired.com/',
     category: 'daily',
-    frequency: 1
+    frequency: 1,
   },
   {
     name: 'theguardian',
     url: 'https://www.theguardian.com/international',
     category: 'general',
-    frequency: 3
+    frequency: 3,
   },
   {
     name: 'arxiv-sanity',
     url: 'http://www.arxiv-sanity.com/',
     category: 'daily',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'fast.ai',
     url: 'https://www.fast.ai/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'washingtonpost',
     url: 'https://www.washingtonpost.com/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'nytimes',
     url: 'https://www.nytimes.com/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'github',
     url: 'https://github.com/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'thegradient',
     url: 'https://thegradient.pub',
     category: 'daily',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'reddit',
     url: 'https://www.reddit.com/r/MachineLearning/',
     category: 'daily',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'discordapp',
     url: 'https://discordapp.com/channels/514337135491416065/555093365742501928',
     category: 'errands',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'spearhead',
     url: 'https://spearhead.co/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'crunchbase',
     url: 'https://www.crunchbase.com/',
     category: 'general',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'hbr',
     url: 'https://hbr.org/',
     category: 'thought_provoking',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'paperswithcode',
     url: 'https://paperswithcode.com/sota',
     category: 'ML/DL',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'mix',
     url: 'https://mix.com/',
     category: 'misc',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'codeforces',
     url: 'http://codeforces.com/',
     category: 'interesting',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'iforcedabot',
     url: 'https://iforcedabot.com/',
     category: 'ML/DL',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'deephunt',
     url: 'https://deephunt.in/',
     category: 'ML/DL',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'code.google',
     url: 'https://code.google.com/codejam/contest/3214486/dashboard',
     category: 'interesting',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'csa.iisc.ac',
     url: 'https://www.csa.iisc.ac.in/sem-evts/seminars.php',
     category: 'important',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'workflowy',
     url: 'https://workflowy.com/',
     category: 'important',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'forbes',
     url: 'https://www.forbes.com',
     category: 'thought_provoking',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'buzzfeed',
     url: 'https://www.buzzfeed.com/',
     category: 'interesting',
-    frequency: 7
+    frequency: 7,
   },
   {
     name: 'madewithml',
     url: 'https://madewithml.com/',
     category: 'ML/DL',
-    frequency: 7
-  }
+    frequency: 7,
+  },
 ];
