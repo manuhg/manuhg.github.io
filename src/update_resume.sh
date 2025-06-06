95e9e7ef290abb056cd177411c026a77d520fb19
Manu Hegde <manuhegdev@gmail.com>
2025-06-06 13:42:04 -0700

    update resume

---
+++ src/update_resume.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp -v ~/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/share-icloud/resume-share/Resume-Manu_Hegde.pdf \
+~/dev/personal/manuhg.github.io/src/Resume-Manu_Hegde.pdf
